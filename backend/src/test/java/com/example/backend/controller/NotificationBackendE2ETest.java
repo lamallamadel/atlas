9a61f9344eb71eb7a748c9254dc99ea659193b76
Adel Lamallam <adel.lamallam.external@eviden.com>
2026-01-09 05:49:26 +0100

    changing doThrow(new MessagingException(...)) to doThrow(new RuntimeException

--- backend/src/test/java/com/example/backend/controller/NotificationBackendE2ETest.java
+++ backend/src/test/java/com/example/backend/controller/NotificationBackendE2ETest.java
@@ -220,7 +220,7 @@ class NotificationBackendE2ETest extends BaseBackendE2ETest {
     void processPendingNotifications_WithFailedEmail_TransitionsToFailedStatus() throws Exception {
         MimeMessage mockMessage = mock(MimeMessage.class);
         when(mailSender.createMimeMessage()).thenReturn(mockMessage);
-        doThrow(new MessagingException("SMTP connection failed")).when(mailSender).send(any(MimeMessage.class));
+        doThrow(new RuntimeException("SMTP connection failed")).when(mailSender).send(any(MimeMessage.class));
 
         NotificationEntity notification = new NotificationEntity();
         notification.setOrgId(ORG_ID);
