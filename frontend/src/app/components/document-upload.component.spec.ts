39390d1bd66e3e79eb43d3bf8e73625453de5ca0
Adel Lamallam <adel.lamallam.external@eviden.com>
2026-01-22 13:05:58 +0100

    Add HttpClientTestingModule to TestBed imports in document-upload.component.spec.ts

--- frontend/src/app/components/document-upload.component.spec.ts
+++ frontend/src/app/components/document-upload.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DocumentUploadComponent } from './document-upload.component';
 
 describe('DocumentUploadComponent', () => {
@@ -7,7 +8,8 @@ describe('DocumentUploadComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DocumentUploadComponent]
+      declarations: [DocumentUploadComponent],
+      imports: [HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(DocumentUploadComponent);
     component = fixture.componentInstance;
