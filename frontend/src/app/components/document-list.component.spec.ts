986ce882ee32d5b3246e388a72c769f4f6cd41ae
Adel Lamallam <adel.lamallam.external@eviden.com>
2026-01-22 12:55:48 +0100

    Add HttpClientTestingModule to TestBed imports in document-list.component.spec.ts

--- frontend/src/app/components/document-list.component.spec.ts
+++ frontend/src/app/components/document-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DocumentListComponent } from './document-list.component';
 
 describe('DocumentListComponent', () => {
@@ -7,7 +8,8 @@ describe('DocumentListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DocumentListComponent]
+      declarations: [DocumentListComponent],
+      imports: [HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(DocumentListComponent);
     component = fixture.componentInstance;
