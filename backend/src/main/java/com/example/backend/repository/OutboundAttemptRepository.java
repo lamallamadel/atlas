98a03ab0f2622e3f35fdf5ff4fd291e217840ad3
Adel Lamallam <adel.lamallam.external@eviden.com>
2026-01-24 15:55:05 +0100

    Fix compilation error in ObservabilityService.java:88 where findDeliveryLatencies returns List OutboundAttemptEntity; but is assigned to List of Double

--- backend/src/main/java/com/example/backend/repository/OutboundAttemptRepository.java
+++ backend/src/main/java/com/example/backend/repository/OutboundAttemptRepository.java
@@ -2,7 +2,6 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.OutboundAttemptEntity;
 import com.example.backend.entity.enums.MessageChannel;
-import com.example.backend.entity.enums.OutboundAttemptStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,16 +12,24 @@ import java.util.List;
 
 @Repository
 public interface OutboundAttemptRepository extends JpaRepository<OutboundAttemptEntity, Long> {
-    
+
     List<OutboundAttemptEntity> findByOutboundMessageIdOrderByAttemptNoAsc(Long outboundMessageId);
-    
+
     @Query("SELECT oa FROM OutboundAttemptEntity oa " +
            "JOIN oa.outboundMessage om " +
            "WHERE om.channel = :channel AND oa.status = 'SUCCESS' AND oa.createdAt >= :afterTime")
     List<OutboundAttemptEntity> findSuccessfulAttempts(@Param("channel") MessageChannel channel, @Param("afterTime") LocalDateTime afterTime);
-    
-    @Query("SELECT oa FROM OutboundAttemptEntity oa " +
-           "JOIN oa.outboundMessage om " +
-           "WHERE om.channel = :channel AND oa.status = 'SUCCESS' AND oa.createdAt >= :afterTime")
-    List<OutboundAttemptEntity> findDeliveryLatencies(@Param("channel") MessageChannel channel, @Param("afterTime") LocalDateTime afterTime);
+
+    @Query("""
+    SELECT (1.0 * timestampdiff( second ,oa.createdAt, oa.updatedAt) ) AS deliveryLatency
+    FROM OutboundAttemptEntity oa
+    JOIN oa.outboundMessage om
+    WHERE om.channel = :channel
+      AND oa.status = 'SUCCESS'
+      AND oa.createdAt >= :afterTime
+      AND oa.updatedAt IS NOT NULL
+""")
+    List<Double> findDeliveryLatencies(@Param("channel") MessageChannel channel,
+                                       @Param("afterTime") LocalDateTime afterTime);
+
 }
